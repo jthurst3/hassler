2ba1f9f39cb6004f087010ffc343b4873f036bd9
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-01-14 19:10:14 -0500

    or not

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Hassler::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( *.css *.js )
+  config.assets.precompile += ['connectfour.css', 'game.js']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
