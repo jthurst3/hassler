57a5d523b662836cc54d7833ab367e55e9f49baf
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-04-26 12:58:07 -0400

    Revert "testing without precompiling assets"
    This reverts commit 063fde8070c96fe1d4756340280e94504dc43559.
    
    back 3 commits

--- config/application.rb
+++ config/application.rb
@@ -27,16 +27,16 @@ module Hassler
     # via https://github.com/sstephenson/sprockets/issues/347#issuecomment-25543201
 
     # We don't want the default of everything that isn't js or css, because it pulls too many things in
-    # config.assets.precompile.shift
+    config.assets.precompile.shift
 
-    # # Explicitly register the extensions we are interested in compiling
-    # config.assets.precompile.push(Proc.new do |path|
-    #   File.extname(path).in? [
-    #     '.html', '.erb', '.haml',                 # Templates
-    #     '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
-    #     '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-    #     '.txt' # everything else
-    #   ]
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml',                 # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        '.txt' # everything else
+      ]
     end)
   end
 end
