d8a2bab5e284dbcbe9862e560909ea98955418a4
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-04-25 22:10:07 -0400

    add image, migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423032404) do
+ActiveRecord::Schema.define(version: 20140426020136) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140423032404) do
     t.text     "caption"
     t.string   "url"
     t.string   "file"
+    t.string   "image_caption", limit: nil
   end
 
   create_table "projects", force: true do |t|
