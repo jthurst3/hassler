442206fb7e84454a6e0302f9766087938394bddf
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-05-04 14:52:48 -0400

    add rack-rewrite to Gemfile to eliminate trailing slashes

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module Hassler
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # from http://work.stevegrossi.com/2013/02/21/remove-trailing-slashes-in-urls-with-rails/
+    config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+      r301 %r{^/(.*)/$}, '/$1'
+    end
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
